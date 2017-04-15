51ef9e667858f5127fe3716743f02df3b85f842b
Jordan Mavrogeorge <jordomav@gmail.com>
2017-04-14 20:34:29 -0700

    set up a linux build file as well

---
+++ ionic-build-linux.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "-----Installing Deps-----"
+sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+
+if ! brew -v
+echo "-----Installing Linuxbrew-----"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+fi
+
+if ! node -v
+echo "-----Installing Node.js-----"
+brew install node
+fi
+
+echo "-----Installing Ionic and Ionic Deps-----"
+npm i -g gulp nodemon ionic cordova
